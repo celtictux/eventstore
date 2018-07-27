6287e9a95e6a820dcee26aa8ccdeb8c8c62075d5
celtictux <adamkelso@gmail.com>
2018-07-27 13:10:48 +0100

    First commit

--- src/main/java/org/celtictux/eventstore/EventStore.java
+++ src/main/java/org/celtictux/eventstore/EventStore.java
@@ -1,12 +1,14 @@
 package org.celtictux.eventstore;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @RestController
 public class EventStore {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot!";
     }

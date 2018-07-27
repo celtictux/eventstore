f470d3c3d0da5d88e582c67738d96d5cc5e07634
celtictux <adamkelso@gmail.com>
2018-07-27 13:45:01 +0100

    First commit

--- src/main/java/org/celtictux/eventstore/EventStore.java
+++ src/main/java/org/celtictux/eventstore/EventStore.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController
+@RestController("/")
 public class EventStore {
 
     @RequestMapping("/hello")

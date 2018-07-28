6c0216b897382411a766761e689b259eefe4cb1e
celtictux <adamkelso@gmail.com>
2018-07-28 13:23:50 +0100

    First commit

--- src/main/java/org/celtictux/eventstore/EventStore.java
+++ src/main/java/org/celtictux/eventstore/EventStore.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @Controller
-@RestController("/events")
+@RestController("events")
 public class EventStore {
 
     @Autowired

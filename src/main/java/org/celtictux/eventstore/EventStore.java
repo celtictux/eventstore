eedf252fd20e1a8f3285b5535bbed03b11741113
celtictux <adamkelso@gmail.com>
2018-07-28 15:56:55 +0100

    Fix issue with HTTP annotation

--- src/main/java/org/celtictux/eventstore/EventStore.java
+++ src/main/java/org/celtictux/eventstore/EventStore.java
@@ -28,8 +28,8 @@ public class EventStore {
     }
 
     @RequestMapping(value = "/addEvent", method = RequestMethod.POST)
-    public void addEvent(@PathVariable("eventName")  final String eventName,
-                         @PathVariable("eventLocation") final String eventLocation) {
+    public void addEvent(@RequestBody final String eventName,
+                         @RequestBody final String eventLocation) {
         Event event = new Event();
         event.setEventName(eventName);
         event.setEventLocation(eventLocation);
